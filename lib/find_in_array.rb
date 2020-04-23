cae9f205ff3547a4c0f285d80b4fd08252e920f9
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-23 17:56:03 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -5,7 +5,8 @@
 def find_element_index(array, value_to_find)
   i = 0
   while i < array.length do 
-  if array[i] == value_to_find
-    return
+    if array[i] == value_to_find
+      return 
+    end
   end
 end
