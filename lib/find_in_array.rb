f197dd4dbd388864ca9cefa4f39f953363f34c39
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-23 17:54:52 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,10 @@
 # def find_element_index(array, value_to_find)
 #   array.index(value_to_find)
 # end
+
+def find_element_index(array, value_to_find)
+  i = 0
+  while i < array.length do 
+  
+end
+end
